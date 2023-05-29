169ac1d97ea2aa7272caa50c3630136a84d77ec6
pedropauloafk <pedropaulo.afk@gmail.com>
2023-05-29 14:05:14 -0300

    3.0

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,52 @@
+''
+
+   const pegaRaca = document.getElementById('id_cachorro');
+     
+   function pegaDados(){
+        var selecionaRaca = pegaRaca.value;
+        const buscaDados = `https://dog.ceo/api/breed/${selecionaRaca}/images/random/5`
 
 
-const response = fetch('https://dog.ceo/api/breeds/image/random');
-console.log(response);
+        fetch (buscaDados)
+        .then(function(resposta){
+            return resposta.json();
+
+        } )
+        .then(function(cachorro) {
+
+            for ( var i = 1; i <6; i++){
+
+                mostraFoto(cachorro.message[i - 1],i)
+
+            }
+            
+
+        })
+
+        .catch( function (erro){
+
+            console.log(" Apresentou erro" + erro)
+
+        });
+
+    }
+
+
+    function mostraFoto(imagem,i){
+
+        document.getElementById('imagem'+ i).src = imagem;
+        
+
+
+
+    }
+
+
+
+
+
+    
+
+    
+
+
